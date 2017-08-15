66f4d1c275d38ddeaec68cf2b85d946d80843b0b
Christoph Franke <krito@krito.de>
2017-08-15 17:24:13 +0200

    made preview thumbs adapt to certain sizes

--- www/site/snippets/artist-content.php
+++ www/site/snippets/artist-content.php
@@ -1,5 +1,34 @@
+<?php
+	$image = $page->image();
+	$ratio = $image->dimensions()->ratio();
+	if($ratio > 2){
+		//landscape
+		$image_class = 'fullwidth';
+		$thumbnail_config = array(
+			'width' => 1200,
+		);
+	}
+	if($ratio <= 2 && $ratio > 1){
+		//cubic
+		$image_class = 'halfwidth';
+		$thumbnail_config = array(
+			'width' => 600,
+			'height' => 500,
+			'crop' => true
+		);				
+	}
+	if($ratio <= 1){
+		//portrait
+		$image_class = 'halfwidth';
+		$thumbnail_config = array(
+			'width' => 600,
+			'height' => 800,
+			'crop' => true
+		);		
+	}
+?>
 <div class="artist-content">
 	<h1><?php echo $page->title()->html() ?></h1>
-	<?php echo $page->image(); ?>
+	<img src="<?php echo thumb($page->image(), $thumbnail_config)->url(); ?>" class="<?php echo $image_class; ?>">
 	<p><?php echo $page->text()->kirbytext(); ?></p>
 </div>
