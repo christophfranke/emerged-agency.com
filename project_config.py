24970808b845bf44f58da8a6d9b7fa05868188a2
Christoph Franke <krito@krito.de>
2017-09-05 19:39:34 +0200

    connected scripts to remote ftp

--- project_config.py
+++ project_config.py
@@ -4,6 +4,15 @@ LOCAL_DOMAIN = 'emerged-relaunch.local'
 LIVE_DOMAIN = 'emerged-agency.com'
 
 
+FTP_USER = 'u81894925-krito'
+FTP_PASSWORD = 'akW9YJWSjd5W'
+#FTP_HOST = 'u81894925.1and1-data.host'
+FTP_PATH_TO_WWW_DIR = 'www'
+FTP_FORCE_IP4 = True
+
+
 ENABLE_BUILD_SYSTEM = True
 BUILD_URL = '/assets/public'
 SRC_URL = '/assets/src'
+
+SECURITY_HASH = 'DH1g5BTSgOoDF67XN5aU33ep4AphPL9H' #added automatically on 2017-09-05 19:27:14.930625
