29865c63fcd7ed9c88fba8a9d2b36fc773ef23b0
Christoph Franke <krito@krito.de>
2017-09-13 13:56:03 +0200

    ignore thumbs and cache

--- project_config.py
+++ project_config.py
@@ -16,3 +16,5 @@ BUILD_URL = '/assets/public'
 SRC_URL = '/assets/src'
 
 SECURITY_HASH = 'DH1g5BTSgOoDF67XN5aU33ep4AphPL9H' #added automatically on 2017-09-05 19:27:14.930625
+
+IGNORE_ON_SYNC = ['REMOTE_COMMAND_FILE', 'TMP_DIR', 'wp-config-local.php', '.DS_Store', '._*', '.gitignore', 'wp-content/cache/', '.htaccess.custom', '.htaccess.local', 'thumbs/*', 'site/cache/*']
