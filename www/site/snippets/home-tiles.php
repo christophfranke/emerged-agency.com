75981e1bfc2317dfa82eceab46e7eb6fba92ecbf
Henning Kuehl <henning@kuehlonline.de>
2023-11-27 17:48:19 +0100

    Update home-tiles.php

--- www/site/snippets/home-tiles.php
+++ www/site/snippets/home-tiles.php
@@ -13,7 +13,7 @@
 	{
 		if($artist->hasImages()){
 			$visible = true;			
-			if(isset($letter) && $letter != substr($artist->uid(), 0, 1))
+			if(!empty($letter) && $letter != substr($artist->uid(), 0, 1))
 				$visible = false;
 			snippet('home-tile', compact('artist', 'visible'));
 		}
